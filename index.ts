362ac37a0a7b32572a4d0e4782a95889cfa530a8
L.L <98739535+LJS360d@users.noreply.github.com>
2022-11-29 19:41:23 +0100

    a

--- index.ts
+++ index.ts
@@ -1,4 +1,7 @@
-document.getElementById('crypt').onclick = ()=>{
-    console.log('Puppa');
-    document.append(document.getElementById('plaintext').value) 
+const plaintext  = document.getElementById('plaintext') as HTMLInputElement;
+const submitbtn  = document.getElementById('crpyt') as HTMLInputElement;
+
+submitbtn.onclick = ()=>{
+    console.log(plaintext.value);
+    document.append(plaintext.value) 
 }
