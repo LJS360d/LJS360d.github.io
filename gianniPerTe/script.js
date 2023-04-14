a78f34f6d21d4cb85c99d4e7d49f31b775f04cf7
LUCA LENCIONI <luca.lencioni09@gmail.com>
2023-04-14 12:12:07 +0200

    Chat Style

--- gianniPerTe/script.js
+++ gianniPerTe/script.js
@@ -1,20 +1,26 @@
 document.getElementById('msgforgpt').addEventListener('keypress', async function (e) {
     if (e.key == "Enter" && !e.shiftKey) {
+        const contentRight = document.createElement('div')
+        contentRight.className = 'content-right'
         const msgRender = document.createElement('md-block')
         msgRender.textContent = this.value
         msgRender.className = 'gptmsg'
-        document.querySelector('div.content-right').appendChild(msgRender)
+        contentRight.appendChild(msgRender)
+        document.querySelector('div.content').appendChild(contentRight)
         await getGPTResponse(this.value).then(this.value = '')
     }
 })
-document.getElementById('msgsend').onclick = async function(){
+document.getElementById('msgsend').onclick = async function () {
     const msgInputBox = document.getElementById('msgforgpt')
+    const contentRight = document.createElement('div')
+    contentRight.className = 'content-right'
     const msgRender = document.createElement('md-block')
     msgRender.textContent = msgInputBox.value
     msgRender.className = 'gptmsg'
-    document.querySelector('div.content-right').appendChild(msgRender)
+    contentRight.appendChild(msgRender)
+    document.querySelector('div.content').appendChild(contentRight)
     await getGPTResponse(msgInputBox.value).then(msgInputBox.value = '')
-} 
+}
 
 async function getGPTResponse(msg) {
     toggleLoading()
@@ -32,20 +38,23 @@ async function getGPTResponse(msg) {
         .then(response => response.json())
         .then(httpresponse => {
             const responsemsg = httpresponse.choices[0].message.content
+            const contentLeft = document.createElement('div')
+            contentLeft.className = 'content-left'
             const responseRender = document.createElement('md-block')
             responseRender.textContent = responsemsg
             responseRender.className = 'gptmsg'
-            document.querySelector('div.content-left').appendChild(responseRender)
+            contentLeft.appendChild(responseRender)
+            document.querySelector('div.content').appendChild(contentLeft)
             toggleLoading()
         }
         )
         .catch(err => console.error(err));
 }
-function toggleLoading(){
+function toggleLoading() {
     const dotContainer = document.querySelector('.dot-container')
-    if(dotContainer.classList.contains('paused')){
+    if (dotContainer.classList.contains('paused')) {
         dotContainer.classList.remove('paused')
-    }else{
+    } else {
         dotContainer.classList.add('paused')
 
     }
