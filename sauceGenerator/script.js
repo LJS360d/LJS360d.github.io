40f01ad6620f075ccc937c7bc4dfbc109bea878a
LUCA LENCIONI <luca.lencioni09@gmail.com>
2023-04-14 06:46:04 +0200

    Clean Clean

--- sauceGenerator/script.js
+++ sauceGenerator/script.js
@@ -11,12 +11,6 @@ const sendFunction = sendButton.onclick = async () => {
     const category = categorySelect.value
     await getWaifuPic(type, category)
 }
-document.addEventListener('keypress',(e)=>{
-    if(e.key == "Enter"){
-        sendFunction()
-    }
-})
-
 async function getWaifuPic(type, category) {
     const response = (await fetch(`https://api.waifu.pics/${type}/${category}`)).json()
     response.then(res => {
