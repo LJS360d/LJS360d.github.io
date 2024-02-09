84f863d12c2857569a15e861c998bd4784bb4f76
LJS360d <luca.lencioni09@gmail.com>
2024-02-09 21:25:34 +0100

    tweak: sticky navbar

--- src/components/layout/Toolbar.tsx
+++ src/components/layout/Toolbar.tsx
@@ -1,4 +1,4 @@
-import { MdMenu } from 'react-icons/md';
+import { MdMenu } from "react-icons/md";
 
 interface ToolbarProps {
   title: string;
@@ -7,15 +7,15 @@ interface ToolbarProps {
 
 export default function Toolbar({ title, sidebarId }: ToolbarProps) {
   return (
-    <header className='navbar bg-base-300 shadow-lg'>
-      <div className='navbar-start gap-2'>
-        <label htmlFor={sidebarId} className='btn rounded-full btn-ghost'>
-          <MdMenu className='text-lg' />
+    <header className="navbar bg-base-300 shadow-lg sticky top-0 z-50">
+      <div className="navbar-start gap-2">
+        <label htmlFor={sidebarId} className="btn rounded-full btn-ghost">
+          <MdMenu className="text-lg" />
         </label>
-        <h1 className='select-none'>{title}</h1>
+        <h1 className="select-none">{title}</h1>
       </div>
-      <div className='navbar-center' />
-      <div className='navbar-end' />
+      <div className="navbar-center" />
+      <div className="navbar-end" />
     </header>
   );
 }
